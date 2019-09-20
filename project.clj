e13be0e77091c255e0ad9c307a008a7ef20d1e9e
Shogo Ohta <athos0220@gmail.com>
2019-09-20 22:05:47 +0900

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject postmortem "0.1.0-SNAPSHOT"
-  :description "A tiny Clojure debugging tool for logging locals' snapshots flexibly, inspired by scope-capture"
-  :url "http://example.com/FIXME"
+  :description "A tiny programmable debugging logger for Clojure(Script), powered by transducers"
+  :url "https://github.com/athos/postmortem"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]])
