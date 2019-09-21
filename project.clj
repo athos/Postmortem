ec50e63efd96f9abeca0b2d49fe2f44bfb71af90
OHTA Shogo <athos0220@gmail.com>
2019-09-21 13:42:22 +0900

    Update deps

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "https://github.com/athos/postmortem"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]])
+  :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
+                 [org.clojure/clojurescript "1.10.520" :scope "provided"]]
+  :profiles {:1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}})
