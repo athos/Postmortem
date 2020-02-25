e35225ffa9fe6724b6e0abbbafe5645775d689fb
Shogo Ohta <athos0220@gmail.com>
2020-02-25 20:48:21 +0900

    Prepare for next development iteration (0.4.1-SNAPSHOT)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject postmortem "0.4.0"
+(defproject postmortem "0.4.1-SNAPSHOT"
   :description "A tiny value-oriented debugging tool for Clojure(Script), powered by transducers"
   :url "https://github.com/athos/Postmortem"
   :license {:name "Eclipse Public License"
