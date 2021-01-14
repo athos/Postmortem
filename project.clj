7661e696d9557dba7f4b1373f65030e44873ee95
Shogo Ohta <athos0220@gmail.com>
2021-01-14 22:45:53 +0900

    Release 0.4.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject postmortem "0.4.1-SNAPSHOT"
+(defproject postmortem "0.4.1"
   :description "A tiny value-oriented debugging tool for Clojure(Script), powered by transducers"
   :url "https://github.com/athos/Postmortem"
   :license {:name "Eclipse Public License"
