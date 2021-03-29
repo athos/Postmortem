65669ff35a918ae7edfbbc84138a7cd5f3dec6b2
Shogo Ohta <athos0220@gmail.com>
2021-03-29 23:44:15 +0900

    Bump deps

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "https://github.com/athos/Postmortem"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
-                 [org.clojure/clojurescript "1.10.520" :scope "provided"]
+  :dependencies [[org.clojure/clojure "1.10.3" :scope "provided"]
+                 [org.clojure/clojurescript "1.10.773" :scope "provided"]
                  [net.cgrand/macrovich "0.2.1"]]
-  :plugins [[lein-eftest "0.5.8"]]
+  :plugins [[lein-eftest "0.5.9"]]
   :eftest {:multithread? :vars}
   :profiles {:1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}})
