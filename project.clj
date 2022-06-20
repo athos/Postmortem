c3403cb94eec1ea6bebaaa63fc8b66c802aa25eb
Shogo Ohta <athos0220@gmail.com>
2022-06-20 22:57:58 +0900

    Fix wordings

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject postmortem "0.5.1-SNAPSHOT"
-  :description "A tiny data-oriented debugging tool for Clojure(Script), empowered by transducers"
+  :description "A tiny data-oriented debugging tool for Clojure(Script), powered by transducers"
   :url "https://github.com/athos/Postmortem"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
