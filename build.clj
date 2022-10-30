d0dea3fe1c1d8ed8c1a8b8bb88958ad6804ca422
Shogo Ohta <athos0220@gmail.com>
2022-10-30 15:46:46 +0900

    Migrate to build-clj

---
+++ build.clj
@@ -0,0 +1,27 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]
+            [org.corfield.build :as bb]))
+
+(def lib 'postmortem/postmortem)
+(def version "0.5.2-SNAPSHOT")
+(def tag (b/git-process {:git-args "rev-parse HEAD"}))
+
+(defn clean [opts]
+  (bb/clean opts))
+
+(defn jar [opts]
+  (-> opts
+      (assoc :src-pom "template/pom.xml"
+             :lib lib :version version :scm {:tag tag})
+      (clean)
+      (bb/jar)))
+
+(defn install [opts]
+  (-> opts
+      (assoc :lib lib :version version)
+      (bb/install)))
+
+(defn deploy [opts]
+  (-> opts
+      (assoc :lib lib :version version)
+      (bb/deploy)))
